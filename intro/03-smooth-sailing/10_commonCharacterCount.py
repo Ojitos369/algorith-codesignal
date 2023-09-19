cba5be9f40a179f583e919e4ca9a7f2d8e5dd7a5
ojitos369 <ojitos369@gmail.com>
2023-09-19 17:57:45 -0600

    init intro 10

--- intro/03-smooth-sailing/10_commonCharacterCount.py
+++ intro/03-smooth-sailing/10_commonCharacterCount.py
@@ -1,18 +1,24 @@
 """
-Description of problem
+Given two strings, find the number of common characters between them.
+
+Example
+
+For s1 = "aabcc" and s2 = "adcaa", the output should be
+solution(s1, s2) = 3.
+
+Strings have 3 common characters - 2 "a"s and 1 "c".
 """
 
-def solution(d): # d of data
+def solution(s1, s2): # d of data
     print()
-    print(d)
-    ...
-    return "correct_answer"
+    print(s1, s2)
+    return sum([int(s1[i] == s2[i]) for i in range(len(s1))])
 
 
 def main():
-    data = "input_data"
-    r = solution(data)
-    sol = "correct_answer"
+    data = ["aabcc", "adcaa"]
+    r = solution(*data)
+    sol = 3
     print(f"{sol} -> {r} \t {sol == r}")
 
 
