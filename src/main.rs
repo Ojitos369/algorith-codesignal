b3b9189ba2a3cbcb27546d7275d8de8b4d737f10
ojitos369 <ojitos369@gmail.com>
2023-09-20 15:24:48 -0600

    init 9

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,42 @@
 /*
-After becoming famous, the CodeBots decided to move into a new building together. Each of the rooms has a different cost, and some of them are free, but there's a rumour that all the free rooms are haunted! Since the CodeBots are quite superstitious, they refuse to stay in any of the free rooms, or any of the rooms below any of the free rooms.
+Given an array of strings, return another array containing all of its longest strings.
 
-Given matrix, a rectangular matrix of integers, where each value represents the cost of the room, your task is to return the total sum of all rooms that are suitable for the CodeBots (ie: add up all the values that don't appear below a 0).
+Example
+
+For inputArray = ["aba", "aa", "ad", "vcd", "aba"], the output should be
+solution(inputArray) = ["aba", "vcd", "aba"].
 */
 
-fn solution(d: Vec<Vec<i32>>) -> i32 {
+fn solution(d: Vec<String>) -> Vec<String> {
     println!();
     println!("{:?}", d);
-    
-    let mut tot = 0;
-    let mut cer: Vec<i32> = vec![];
-    for i in 0..d.len() {
-        for j in 0..d[i].len() {
-            if d[i][j] == 0 {
-                cer.push(j as i32);
-            }
-            if !cer.contains(&(j as i32)) {
-                tot += d[i][j]
-            }
-        }
-    }
-    return tot
+
+    let mut max_len = 0;
+    // .iter() -> creates an iterator over a slice
+    // .fold() -> applies a function to each element of an iterator, passing the result of each such invocation into the next
+    // acc -> accumulator
+    // s -> current value
+    max_len = d.iter().fold(max_len, |acc, s| if s.len() > acc { s.len() } else { acc });
+    // d.iter().fold(max_len, |acc, s| if s.len() > acc { s.len() } else { acc });
+    // rs = [s for s in d if len(s) == ml]
+
+    let mut rs: Vec<String> = Vec::new();
+    rs = d.iter().fold(rs, |acc, s| if s.len() == max_len { acc.push(s.to_string()); acc.to_vec() } else { acc });
+    rs
+
 }
 
 fn main() {
     let tests = [
-        [[0, 1, 1, 2], 
-        [0, 5, 0, 0], 
-        [2, 0, 3, 3]]
+        ["aba", "aa", "ad", "vcd", "aba"]
+    ];
+    
+    let sols = [
+        ["aba", "vcd", "aba"]
     ];
 
-    let sols = [9];
-
     for i in 0..tests.len() {
-        let d = tests[i].to_vec().iter().map(|x| x.to_vec()).collect();
+        let d = tests[i].to_vec();
         let r = solution(d);
         println!("{} -> {} \t {}", sols[i], r, sols[i] == r);
     }
