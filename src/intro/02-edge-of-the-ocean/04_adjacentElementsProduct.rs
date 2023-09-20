3303ff5092744a8d84247cc0e88f18d83ddf896c
ojitos369 <ojitos369@gmail.com>
2023-09-20 12:49:27 -0600

    intro 04

--- src/intro/02-edge-of-the-ocean/04_adjacentElementsProduct.rs
+++ src/intro/02-edge-of-the-ocean/04_adjacentElementsProduct.rs
@@ -1,4 +1,4 @@
-"""
+/*
 Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
 
 Example
@@ -7,14 +7,27 @@ For inputArray = [3, 6, -2, -5, 7, 3], the output should be
 solution(inputArray) = 21.
 
 7 and 3 produce the largest product.
-"""
+*/
+
+fn solution(d: Vec<i32>) -> i32 {
+    if d.len() > 2 {
+        return std::cmp::max(d[0]*d[1], solution(d[1..].to_vec()));
+    } else {
+        return d[0]*d[1];
+    }
+}
 
 
-def solution(l):
-    return max(l[0]*l[1], solution(l[1:])) if len(l) > 2 else l[0]*l[1]
 
+fn main() {
+    let tests = [
+        [3, 6, -2, -5, 7, 3],
+    ];
+    let sols = [21];
 
-if __name__ == '__main__':
-    inputArray = [3, 6, -2, -5, 7, 3]
-    r = solution(inputArray)
-    print(r)
+    for i in 0..tests.len() {
+        let d = tests[i].clone().to_vec();
+        let r = solution(d);
+        println!("{} -> {} \t {}", sols[i], r, sols[i] == r);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,32 @@
 /*
-Given the string, check if it is a palindrome.
+Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
 
 Example
 
-For inputString = "aabaa", the output should be
-solution(inputString) = true;
-For inputString = "abac", the output should be
-solution(inputString) = false;
-For inputString = "a", the output should be
-solution(inputString) = true.
+For inputArray = [3, 6, -2, -5, 7, 3], the output should be
+solution(inputArray) = 21.
+
+7 and 3 produce the largest product.
 */
 
-fn solution(d: String) -> bool {
-    // .chars() returns an iterator over the characters of a string slice
-    // .rev() reverses the iterator
-    // .collect::<String>() collects the iterator into a String
-    d == d.chars().rev().collect::<String>()
+fn solution(d: Vec<i32>) -> i32 {
+    if d.len() > 2 {
+        return std::cmp::max(d[0]*d[1], solution(d[1..].to_vec()));
+    } else {
+        return d[0]*d[1];
+    }
 }
 
 
 
 fn main() {
     let tests = [
-        String::from("aabaa"),
-        String::from("abac"),
-        String::from("a")
+        [3, 6, -2, -5, 7, 3],
     ];
-    let sols = [true, false, true];
+    let sols = [21];
 
     for i in 0..tests.len() {
-        let d = tests[i].clone();
+        let d = tests[i].clone().to_vec();
         let r = solution(d);
         println!("{} -> {} \t {}", sols[i], r, sols[i] == r);
     }
