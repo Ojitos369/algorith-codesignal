bc4c760457c1d10e7e9752c6f9eeb55aac6601be
ojitos369 <ojitos369@gmail.com>
2023-09-20 17:17:57 -0600

    improve inter 10

--- intro/03-smooth-sailing/10_commonCharacterCount.py
+++ intro/03-smooth-sailing/10_commonCharacterCount.py
@@ -12,12 +12,13 @@ Strings have 3 common characters - 2 "a"s and 1 "c".
 def solution(s1, s2): # d of data
     print()
     print(s1, s2)
-    checked = []
-    def add(c, c1, c2):
-        nonlocal checked
-        checked.append(c)
-        return c1 if c1 - c2 <= 0 else c2
-    return sum( add(c, s1.count(c), s2.count(c)) if c not in checked else 0 for c in s1)
+    return sum(min(s1.count(c), s2.count(c)) for c in set(s1))
+    # checked = []
+    # def add(c, c1, c2):
+    #     nonlocal checked
+    #     checked.append(c)
+    #     return c1 if c1 - c2 <= 0 else c2
+    # return sum( add(c, s1.count(c), s2.count(c)) if c not in checked else 0 for c in s1)
 
 
 def main():
