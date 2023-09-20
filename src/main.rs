d6260ca013ca9b7f8ec8158994842f15b5550d32
ojitos369 <ojitos369@gmail.com>
2023-09-20 17:38:51 -0600

    inter 10

--- src/main.rs
+++ src/main.rs
@@ -9,22 +9,36 @@ solution(inputArray) = ["aba", "vcd", "aba"].
 */
 
 fn solution(s1: String, s2: String) -> i32 {
-
+    let mut total = 0;
+    let a1: Vec<char> = s1.chars().collect();
+    let a2: Vec<char> = s2.chars().collect();
+    let mut checked: Vec<char> = vec![];
+    for i in 0..a1.len() {
+        let char_i = a1[i];
+        if checked.contains(&char_i) {
+            continue;
+        }
+        let count_1 = a1.iter().filter(|&x| *x == char_i).count();
+        let count_2 = a2.iter().filter(|&x| *x == char_i).count();
+        total += std::cmp::min(count_1, count_2) as i32;
+        checked.push(char_i);
+    }
+    total
 }
 
 fn main() {
     let tests = [
-        ["aba", "aa", "ad", "vcd", "aba"]
+        ["aabcc", "adcaa"],
+        ["zzzz", "zzzzzzz"],
+        ["abca", "xyzbac"],
     ];
-    
     let sols = [
-        ["aba", "vcd", "aba"]
+        3, 4, 3
     ];
 
     for i in 0..tests.len() {
-        let d = tests[i].iter().map(|s| s.to_string()).collect();
-        let r = solution(d);
-        let sr: Vec<String> = sols[i].iter().map(|s| s.to_string()).collect();
-        println!("{:?} -> {:?} \t {:?}", sr, r, sr == r);
+        let d = tests[i];
+        let r = solution(d[0].to_string(), d[1].to_string());
+        println!("{:?} -> {:?} \t {:?}", sols[i], r, sols[i] == r);
     }
 }
