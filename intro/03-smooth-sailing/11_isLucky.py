3ce16c0d5faae850c31b6fda266be919c46116d1
ojitos369 <ojitos369@gmail.com>
2024-06-18 16:42:22 -0600

    03-12

--- intro/03-smooth-sailing/11_isLucky.py
+++ intro/03-smooth-sailing/11_isLucky.py
@@ -1,19 +1,29 @@
 """
-Description of problem
+Ticket numbers usually consist of an even number of digits. A ticket number is considered lucky if the sum of the first half of the digits is equal to the sum of the second half.
 """
 
 def solution(d): # d of data
     print()
     print(d)
-    ...
-    return "correct_answer"
+    d = [int(n) for n in str(d)]
+    ld = len(d)
+    md = ld//2
+    return sum(d[0:md]) == sum(d[md:ld])
 
 
 def main():
-    data = "input_data"
-    r = solution(data)
-    sol = "correct_answer"
-    print(f"{sol} -> {r} \t {sol == r}")
+    tests = [
+        1230, 
+        239017
+    ]
+    sols = [
+        True,
+        False
+    ]
+    for i in range(len(tests)):
+        data = tests[i]
+        r = solution(data)
+        print(f"{sols[i]} -> {r} \t {sols[i] == r}")
 
 
 def test():
--- intro/03-smooth-sailing/12_sortByHeight.py
+++ intro/03-smooth-sailing/12_sortByHeight.py
@@ -2,18 +2,41 @@
 Description of problem
 """
 
+def solution_new(d): # d of data
+    fj = d.count(-1)
+    l = sorted([*d])
+    print(l, fj)
+    r = [-1 if n == -1 else l[fj-i] for i,n in enumerate(d)]
+    return r
+
 def solution(d): # d of data
     print()
     print(d)
-    ...
-    return "correct_answer"
+    l = [*d]
+    fj = l.count(-1)
+    l = sorted(l)[fj:]
+    r = []
+    i = 0
+    for n in d:
+        if n == -1:
+            r.append(-1)
+        else:
+            r.append(l[i])
+            i += 1
+    return r
 
 
 def main():
-    data = "input_data"
-    r = solution(data)
-    sol = "correct_answer"
-    print(f"{sol} -> {r} \t {sol == r}")
+    tests = [
+        [-1, 150, 190, 170, -1, -1, 160, 180],
+    ]
+    sols = [
+        [-1, 150, 160, 170, -1, -1, 180, 190],
+    ]
+    for i in range(len(tests)):
+        data = tests[i]
+        r = solution(data)
+        print(f"{sols[i]} -> {r} \t {sols[i] == r}")
 
 
 def test():
